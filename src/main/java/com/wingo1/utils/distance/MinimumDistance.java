f384f77fdf63d7fa64d6f57758c7627f79880e18
zhangxd <wingo1@qq.com>
2019-01-02 10:21:18 +0800

    2点最近距离计算补充上VX VY的计算

--- src/main/java/com/wingo1/utils/distance/MinimumDistance.java
+++ src/main/java/com/wingo1/utils/distance/MinimumDistance.java
@@ -60,9 +60,30 @@ public class MinimumDistance {
 		double Vx2 = Math.cos(degree2) * v2;
 		double Vy1 = Math.sin(degree1) * v1;
 		double Vy2 = Math.sin(degree2) * v2;
+		return getTheTimeByVxVy(x1, y1, Vx1, Vy1, x2, y2, Vx2, Vy2);
+
+	}
+
+	/**
+	 * 根据Vx,Vy的速度来算
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param Vx1
+	 * @param Vy1
+	 * @param x2
+	 * @param y2
+	 * @param Vx2
+	 * @param Vy2
+	 * @return
+	 */
+	private static double getTheTimeByVxVy(double x1, double y1, double Vx1, double Vy1, double x2, double y2,
+			double Vx2, double Vy2) {
+
 		double time = -((2 * (Vx1 - Vx2) * (x1 - x2) + 2 * (Vy1 - Vy2) * (y1 - y2))
 				/ (2 * (Math.pow((Vx1 - Vx2), 2) + Math.pow((Vy1 - Vy2), 2))));
 		return time;
+
 	}
 
 	/**
@@ -87,6 +108,13 @@ public class MinimumDistance {
 		double Vx2 = Math.cos(degree2) * v2;
 		double Vy1 = Math.sin(degree1) * v1;
 		double Vy2 = Math.sin(degree2) * v2;
+		return getTheMinimumDestanceByVxVy(x1, y1, Vx1, Vy1, x2, y2, Vx2, Vy2, t);
+
+	}
+
+	private static double getTheMinimumDestanceByVxVy(double x1, double y1, double Vx1, double Vy1, double x2,
+			double y2, double Vx2, double Vy2, double t) {
+
 		double D = Math.pow((x1 + Vx1 * t) - (x2 + Vx2 * t), 2) + Math.pow((y1 + Vy1 * t) - (y2 + Vy2 * t), 2);
 		return Math.sqrt(D);
 
