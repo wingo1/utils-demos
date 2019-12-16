2fb9590f520b5c1f000faf6bd5514a01423b159f
zhangxd <wingo1@qq.com>
2019-12-16 16:07:06 +0800

    度分秒和十进制转换

---
+++ src/main/java/com/wingo1/example/decimal/DmsAndDeg.java
@@ -0,0 +1,35 @@
+package com.wingo1.example.decimal;
+
+import java.text.DecimalFormat;
+
+/**
+ * 度分秒和十进制转换
+ * 
+ * @author cdatc-wingo1
+ *
+ */
+public class DmsAndDeg {
+	public static String degToDms(double f) {
+		int deg = (int) f;
+		int min = (int) ((f - deg) * 60);
+		float sec = (float) (((f - deg) * 60 - min) * 60);
+		DecimalFormat df = new DecimalFormat("00.000");
+		return deg + "" + min + df.format(sec);
+	}
+
+	public static double dmsTodeg(String dms) {
+		int indexOf = dms.indexOf(".");
+		if (indexOf == -1) {
+			indexOf = dms.length() + 1;
+		}
+		double sec = Double.valueOf(dms.substring(indexOf - 2));
+		int min = Integer.valueOf(dms.substring(indexOf - 4, indexOf - 2));
+		int deg = Integer.valueOf(dms.substring(0, indexOf - 4));
+		return deg + min / 60D + sec / 3600D;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(dmsTodeg("1035648.278"));
+	}
+
+}
